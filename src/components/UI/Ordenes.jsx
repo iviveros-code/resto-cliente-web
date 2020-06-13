6b81be83017f25d5ec0e9da8285f5eec80286cc0
Ivan Viveros <bam@MacBook-Pro-de-Ivan.local>
2020-06-12 23:34:57 -0300

    tiempo de espera

---
+++ src/components/UI/Ordenes.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useContext } from "react";
+import { FirebaseContext } from "../../firebase";
+
+const Orden = ({ orden }) => {
+  const [tiempoentrega, setTiempoEntrega] = useState(0);
+  const { firebase } = useContext(FirebaseContext);
+
+  const definirTiempo = (id) => {
+    try {
+      firebase.db.collection("ordenes").doc(id).update({ tiempoentrega });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="sm:w-1/2 lg:w-1/3 px-2 mb-4">
+      <div className="p-3 shadow-md bg-white">
+        <h1 className="text-yellow-600 text-lg font-bold">{orden.id}</h1>
+        {orden.orden.map((platos) => (
+          <p className="text-gray-600">
+            {platos.cantidad} {platos.nombre}
+          </p>
+        ))}
+        <p className="text-gray-700">Total a pagar : $ {orden.total}</p>
+
+        {orden.tiempoentrega === 0 && (
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2 ">
+              Tiempo de Entrega
+            </label>
+            <input
+              type="number"
+              className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              min="1"
+              max="20"
+              placeholder="20"
+              value={tiempoentrega}
+              onChange={(e) => setTiempoEntrega(parseInt(e.target.value))}
+            />
+            <button
+              onClick={() => definirTiempo(orden.id)}
+              type="submit"
+              className="bg-gray-800 hover:bg-gray-900 w-full mt-5 p-2 text-white uppercase font-bold"
+            >
+              Definir Tiempo
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Orden;
--- src/components/screens/Ordenes.jsx
+++ src/components/screens/Ordenes.jsx
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
+import { FirebaseContext } from "../../firebase";
+import Orden from "../UI/Ordenes";
 
 const Ordenes = () => {
+  const { firebase } = useContext(FirebaseContext);
+
+  const [ordenes, setOrdenes] = useState([]);
+
+  useEffect(() => {
+    const obtenerOrdenes = () => {
+      firebase.db
+        .collection("ordenes")
+        .where("completado", "==", false)
+        .onSnapshot(handleSnapshot);
+    };
+    obtenerOrdenes();
+  }, []);
+
+  function handleSnapshot(snapshot) {
+    const ordenes = snapshot.docs.map((doc) => {
+      return {
+        id: doc.id,
+        ...doc.data(),
+      };
+    });
+    setOrdenes(ordenes);
+  }
+
   return (
     <>
       <h1 className="text-3xl font-light mb-4">Ordenes</h1>
+
+      <div className="sm:flex sm:flex-wrap -mx-3">
+        {ordenes.map((orden) => (
+          <Orden key={orden.id} orden={orden} />
+        ))}
+      </div>
     </>
   );
 };
